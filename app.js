5f285b41671be908943254b1241f3101a5cf5798
Jessica Gonzales <starrbae@Jessicas-MacBook-Air.local>
2021-10-04 14:57:40 -0500

    setup express server app with nodemon edited

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ import express  from "express"
 import logger from "morgan"
 import cors from "cors"
 
-import projectRoutes from '.routes/posts.js'
+import postRoutes from '.routes/posts.js'
 
 const app= express();
 app.use(cors())
 app.use(express.json())
 app.use(logger('dev')) //should ask if dev is necessary or related to gh branch
 
-app.use('/api', projectRoutes);
+app.use('/api', postRoutes);
 export default app 
