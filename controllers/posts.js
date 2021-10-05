77bd62dccb7e866b94acdbe8ed108c47c4234e70
Jessica Gonzales <starrbae@Jessicas-MacBook-Air.local>
2021-10-05 10:25:14 -0500

    added get specific post to controllers posts.js

--- controllers/posts.js
+++ controllers/posts.js
@@ -20,6 +20,20 @@ export const createPost = async (req,res)=>{
     }
 }
 
+export const getPost = async (req,res)=>{
+    try{
+        const {id} = req.params
+        const post = await Post.findById(id)
+        if (post){
+            return res.json(post)
+        }
+        res.status(404).json({message: 'Product not found!'})
+    }catch (error){
+        console.log(error.message)
+        res.status(500).json({error:error.message})
+    }
+}
+
 export const updatePost = async (req,res)=>{
     const {id} = req.params
     const post = await Post.findByIdAndUpdate(id, req.body, {new: true})
